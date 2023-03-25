371c5e662f4d78a70d242775bb6e478856a1460b
Diego Ramos <diegorbrito9@gmail.com>
2023-03-25 13:43:31 -0300

    [backend] feat: criando condição para tratamento de erro do zod

--- backend/src/utils/error-middleware.ts
+++ backend/src/utils/error-middleware.ts
@@ -1,6 +1,7 @@
 import { FastifyError, FastifyReply, FastifyRequest } from "fastify";
 
 import { Exception } from "./Exception";
+import { z as zod } from "zod";
 
 export const errorMiddleware = async (
   error: FastifyError,
@@ -9,6 +10,20 @@ export const errorMiddleware = async (
 ) => {
   if (error instanceof Exception) {
     return response.status(error.statusCode).send({ error: error.message });
+  }
+
+  if (error instanceof zod.ZodError) {
+    const convertError = JSON.parse(error.message)[0];
+
+    const messageError = convertError.message;
+    const errorField = convertError.path[0];
+
+    return response.status(400).send({
+      error: {
+        field: errorField,
+        message: messageError,
+      },
+    });
   }
 
   return response.status(500).send({ error: error.message });
