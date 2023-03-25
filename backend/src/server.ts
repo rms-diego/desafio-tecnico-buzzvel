32835cfb9c3a6d8d7613fe0530534fe81c573f9d
Diego Ramos <diegorbrito9@gmail.com>
2023-03-25 13:39:05 -0300

    [backend] new: criando uma rota para testar se o servidor subiu

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,8 @@ import { app } from "./app";
 
 const { PORT } = process.env;
 
+app.get("/", (_request, response) => response.send("Server up"));
+
 app.listen({ port: +PORT!, host: "0.0.0.0" }, (error, address) => {
   if (error) throw new Error(error.message);
 
